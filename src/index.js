49b35b4dbe9d38ffe408b328353ec49391f432ee
saeedabdilar <saeed.abdilar@gmail.com>
2021-03-19 22:54:46 +0330

    Updates

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import {Spinner} from './components';
-import {MODE, NAME, POSITION} from './configs/variables';
-const POSITIONS = POSITION.LTR;
-
-export {Spinner as default, MODE, NAME, POSITIONS}
